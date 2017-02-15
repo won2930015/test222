1acc13c00ef07baed6eab99a0631b9ad06accf18
won2930015 <58769598@qq.com>
2017-02-15 17:32:49 +0800

    23-1

--- Chapter22/webtest.py
+++ Chapter22/webtest.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Name:        模块1
+# Name:        妯″潡1
 # Purpose:
 #
 # Author:      Administrator
@@ -13,6 +13,7 @@ from xml.sax.handler import ContentHandler
 from xml.sax import parse
 class TestHandler(ContentHandler):
     def startElement(self,name,attrs):
+
         print(name,attrs.keys())
 
 parse('website.xml',TestHandler())
